e82d5be14503e382788ac968953a50c7d4180232
lazbek <jannnjan592@gmail.com>
2022-01-30 15:52:36 +0300

    laazbek

---
+++ js/script.js
@@ -0,0 +1,156 @@
+let first = prompt('birinchi sonni kiriting ?')
+let second = prompt('ikkinchi sonni kiriting ?')
+let third = prompt('uchinchi sonni kiriting ?')
+let fourth = prompt('tortinchi sonni kiriting ?')
+let fifth = prompt('beshinchi sonni kiriting ?')
+let sixth = prompt('oltinchi sonni kiriting ?')
+let seventh = prompt('yettinchi sonni kiriting ?')
+let eightth = prompt('sakkizinchi sonni kiriting ?')
+let nineth = prompt('toqqizinchi sonni kiriting ?')
+let tenth = prompt('oninchi  sonni kiriting ?')
+
+let spanFirst = document.querySelectorAll('.lazbe__span1')[0],
+    spanSecond = document.querySelectorAll('.lazbe__span2')[0],
+    spanThird = document.querySelectorAll('.lazbe__span3')[0],
+    spanfourth = document.querySelectorAll('.lazbe__span4')[0],
+    spanFifth = document.querySelectorAll('.lazbe__span5')[0],
+    spansexth = document.querySelectorAll('.lazbe__span6')[0],
+    spanSeventth = document.querySelectorAll('.lazbe__span7')[0],
+    spanEightth = document.querySelectorAll('.lazbe__span8')[0],
+    spanNineth = document.querySelectorAll('.lazbe__span9')[0],
+    spanTenth = document.querySelectorAll('.lazbe__span10')[0];
+   
+    
+let textFirst = document.querySelectorAll('.lazbek__text1')[0],
+    textSecond = document.querySelectorAll('.lazbek__text2')[0],
+    textThird = document.querySelectorAll('.lazbek__text3')[0],
+    textFourth = document.querySelectorAll('.lazbek__text4')[0],
+    textFifth = document.querySelectorAll('.lazbek__text5')[0],
+    textSexth = document.querySelectorAll('.lazbek__text6')[0],
+    textSeventh = document.querySelectorAll('.lazbek__text7')[0],
+    textEightth = document.querySelectorAll('.lazbek__text8')[0],
+    textNineth = document.querySelectorAll('.lazbek__text9')[0],
+    textTenth = document.querySelectorAll('.lazbek__text10')[0];
+
+    
+    
+    
+if(first % 2 == 0){
+  spanFirst.textContent = `Siz kiritgan birinchi son ${first}`;
+  
+  textFirst.textContent = 'bu juft son';
+  textFirst.style.color = 'green'
+}else{
+  spanFirst.textContent = `Siz kiritgan birinchi son ${first}`;
+  textFirst.textContent = 'bu toq son';
+  textFirst.style.color = 'red'
+  
+}
+if(second % 2 == 0){
+  spanSecond.textContent = `Siz kiritgan birinchi son ${second}`;
+  
+  textSecond.textContent = 'bu juft son';
+  textSecond.style.color = 'green'
+}else{
+  spanSecond.textContent = `Siz kiritgan birinchi son ${second}`;
+  textSecond.textContent = 'bu toq son';
+  textSecond.style.color = 'red'
+  
+}
+if(third % 2 == 0){
+  spanThird.textContent = `Siz kiritgan birinchi son ${third}`;
+  
+  textThird.textContent = 'bu juft son';
+  textThird.style.color = 'green'
+}else{
+  spanThird.textContent = `Siz kiritgan birinchi son ${third}`;
+  textThird.textContent = 'bu toq son';
+  textThird.style.color = 'red'
+  
+}
+if(fourth % 2 == 0){
+  spanfourth.textContent = `Siz kiritgan birinchi son ${fourth}`;
+  
+  textFourth.textContent = 'bu juft son';
+  textFourth.style.color = 'green'
+}else{
+  spanfourth.textContent = `Siz kiritgan birinchi son ${fourth}`;
+  textFourth.textContent = 'bu toq son';
+  textFourth.style.color = 'red'
+  
+}
+if(fifth % 2 == 0){
+  spanFifth.textContent = `Siz kiritgan birinchi son ${fifth}`;
+  
+  textFifth.textContent = 'bu juft son';
+  textFifth.style.color = 'green'
+}else{
+  spanFifth.textContent = `Siz kiritgan birinchi son ${fifth}`;
+  textFifth.textContent = 'bu toq son';
+  textFifth.style.color = 'red'
+  
+}
+if(sixth % 2 == 0){
+  spansexth.textContent = `Siz kiritgan birinchi son ${sixth}`;
+  
+  textSexth.textContent = 'bu juft son';
+  textSexth.style.color = 'green'
+}else{
+  spansexth.textContent = `Siz kiritgan birinchi son ${sixth}`;
+  textSexth.textContent = 'bu toq son';
+  textSexth.style.color = 'red'
+  
+}     
+if(seventh % 2 == 0){
+  spanSeventth.textContent = `Siz kiritgan birinchi son ${seventh}`;
+  
+  textSeventh.textContent = 'bu juft son';
+  textSeventh.style.color = 'green'
+}else{
+  spanSeventth.textContent = `Siz kiritgan birinchi son ${seventh}`;
+  textSeventh.textContent = 'bu toq son';
+  textSeventh.style.color = 'red'
+  
+}      
+if(eightth % 2 == 0){
+  spanEightth.textContent = `Siz kiritgan birinchi son ${eightth}`;
+  
+  textEightth.textContent = 'bu juft son';
+  textEightth.style.color = 'green'
+}else{
+  spanEightth.textContent = `Siz kiritgan birinchi son ${eightth}`;
+  textEightth.textContent = 'bu toq son';
+  textEightth.style.color = 'red'
+  
+}  
+if(nineth % 2 == 0){
+  spanNineth.textContent = `Siz kiritgan birinchi son ${nineth}`;
+  
+  textNineth.textContent = 'bu juft son';
+  textNineth.style.color = 'green'
+}else{
+  spanNineth.textContent = `Siz kiritgan birinchi son ${nineth}`;
+  textNineth.textContent = 'bu toq son';
+  textNineth.style.color = 'red'
+  
+}  
+if(tenth % 2 == 0){
+  spanTenth.textContent = `Siz kiritgan birinchi son ${tenth}`;
+  
+  textTenth.textContent = 'bu juft son';
+  textTenth.style.color = 'green'
+}else{
+  spanTenth.textContent = `Siz kiritgan birinchi son ${tenth}`;
+  textTenth.textContent = 'bu toq son';
+  textTenth.style.color = 'red'
+  
+}  
+
+
+
+
+
+
+
+
+
